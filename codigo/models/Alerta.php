07fb1806eb809ef8f03386b7fd86a33dd22049ce
Alba Martinez <albateresamg@gmail.com>
2025-01-24 15:24:07 +0100

    cambios2

--- codigo/models/Alerta.php
+++ codigo/models/Alerta.php
@@ -3,90 +3,90 @@
 namespace app\models;
 
 use Yii;
-use yii\db\ActiveRecord;
-use yii\db\ActiveQuery;
 
-class Alerta extends ActiveRecord
+/**
+ * This is the model class for table "alerta".
+ *
+ * @property int $id
+ * @property string $titulo
+ * @property string $descripcion
+ * @property string $fecha_inicio
+ * @property int|null $duracion_estimada
+ * @property int|null $id_lugar
+ * @property string|null $detalles
+ * @property string|null $notas
+ * @property string|null $url_externa
+ * @property string $estado
+ * @property int|null $id_usuario
+ *
+ * @property AlertaEtiqueta[] $alertaEtiquetas
+ * @property Comentario[] $comentarios
+ * @property Etiqueta[] $etiquetas
+ * @property Incidencia[] $incidencias
+ * @property Lugar $lugar
+ * @property Usuario $usuario
+ */
+class Alerta extends \yii\db\ActiveRecord
 {
     /**
-     * Nombre de la tabla asociada.
+     * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'alertas';
+        return 'alerta';
     }
 
     /**
-     * Reglas de validación para los campos del modelo.
+     * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['titulo', 'descripcion', 'fecha_inicio', 'duracion_estimada', 'id_lugar', 'estado', 'id_usuario'], 'required'],
+            [['titulo', 'descripcion', 'fecha_inicio', 'estado'], 'required'],
             [['descripcion', 'detalles', 'notas', 'url_externa'], 'string'],
-            [['fecha_inicio'], 'datetime'],
+            [['fecha_inicio'], 'safe'],
             [['duracion_estimada', 'id_lugar', 'id_usuario'], 'integer'],
             [['titulo'], 'string', 'max' => 255],
             [['estado'], 'string', 'max' => 50],
-            [['id_etiqueta'], 'exist', 'skipOnError' => true, 'targetClass' => Etiqueta::class, 'targetAttribute' => ['id_etiqueta' => 'id']],
+            [['id_lugar'], 'exist', 'skipOnError' => true, 'targetClass' => Lugar::class, 'targetAttribute' => ['id_lugar' => 'id']],
+            [['id_usuario'], 'exist', 'skipOnError' => true, 'targetClass' => Usuario::class, 'targetAttribute' => ['id_usuario' => 'id']],
         ];
     }
 
     /**
-     * Etiquetas para los atributos del modelo.
+     * {@inheritdoc}
      */
     public function attributeLabels()
     {
         return [
             'id' => 'ID',
-            'titulo' => 'Título',
-            'descripcion' => 'Descripción',
-            'fecha_inicio' => 'Fecha de Inicio',
-            'duracion_estimada' => 'Duración Estimada',
-            'id_lugar' => 'Lugar',
+            'titulo' => 'Titulo',
+            'descripcion' => 'Descripcion',
+            'fecha_inicio' => 'Fecha Inicio',
+            'duracion_estimada' => 'Duracion Estimada',
+            'id_lugar' => 'Id Lugar',
             'detalles' => 'Detalles',
             'notas' => 'Notas',
-            'url_externa' => 'URL Externa',
+            'url_externa' => 'Url Externa',
             'estado' => 'Estado',
-            'id_usuario' => 'Usuario Creador',
-            'id_etiqueta' => 'Etiqueta Relacionada',
+            'id_usuario' => 'Id Usuario',
         ];
     }
 
     /**
-     * Relación con la tabla de etiquetas.
+     * Gets query for [[AlertaEtiquetas]].
      *
-     * @return ActiveQuery
+     * @return \yii\db\ActiveQuery|AlertaEtiquetaQuery
      */
-    public function getEtiqueta()
+    public function getAlertaEtiquetas()
     {
-        return $this->hasOne(Etiqueta::class, ['id' => 'id_etiqueta']);
+        return $this->hasMany(AlertaEtiqueta::class, ['id_alerta' => 'id']);
     }
 
     /**
-     * Relación con la tabla de lugares.
+     * Gets query for [[Comentarios]].
      *
-     * @return ActiveQuery
-     */
-    public function getLugar()
-    {
-        return $this->hasOne(Lugar::class, ['id' => 'id_lugar']);
-    }
-
-    /**
-     * Relación con la tabla de usuarios.
-     *
-     * @return ActiveQuery
-     */
-    public function getUsuario()
-    {
-        return $this->hasOne(Usuario::class, ['id' => 'id_usuario']);
-    }
-
-    /**
-     * Relación con los comentarios asociados.
-     *
-     * @return ActiveQuery
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
      */
     public function getComentarios()
     {
@@ -94,13 +94,51 @@ class Alerta extends ActiveRecord
     }
 
     /**
-     * Relación con las etiquetas asociadas.
+     * Gets query for [[Etiquetas]].
      *
-     * @return ActiveQuery
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
      */
     public function getEtiquetas()
     {
-        return $this->hasMany(Etiqueta::class, ['id' => 'id'])
-            ->viaTable('alerta_etiqueta', ['id_alerta' => 'id']);
+        return $this->hasMany(Etiqueta::class, ['id' => 'id_etiqueta'])->viaTable('alerta_etiqueta', ['id_alerta' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Incidencias]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getIncidencias()
+    {
+        return $this->hasMany(Incidencia::class, ['id_alerta' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Lugar]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getLugar()
+    {
+        return $this->hasOne(Lugar::class, ['id' => 'id_lugar']);
+    }
+
+    /**
+     * Gets query for [[Usuario]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getUsuario()
+    {
+        return $this->hasOne(Usuario::class, ['id' => 'id_usuario']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return AlertaQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new AlertaQuery(get_called_class());
     }
 }
