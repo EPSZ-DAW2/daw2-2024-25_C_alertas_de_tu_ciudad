2c9ea87b5047fbaf396115d1586e35046ffb5ba0
Alba Martinez <albateresamg@gmail.com>
2025-02-20 20:47:49 +0100

    cambio de rama

--- sql/db_proyecto_c_AlertasEtiquetasCategorias(3).sql
+++ sql/db_proyecto_c_AlertasEtiquetasCategorias(3).sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 20-01-2025 a las 20:44:59
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `proyecto_c`
---
-
--- --------------------------------------------------------
 
 
 
@@ -149,13 +125,6 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 
-ALTER TABLE usuario
-    ADD COLUMN phone VARCHAR(15) DEFAULT NULL COMMENT 'Número de teléfono del usuario';
-
-ALTER TABLE usuario
-    ADD COLUMN status TINYINT(1) DEFAULT 0 COMMENT 'Estado del usuario'; -- 默认值为 0，表示未确认
-
-
 --
 -- Estructura de tabla para la tabla `configuracion`
 --
@@ -186,12 +155,6 @@ VALUES
     ('pagination_size', '10', 'Número de elementos por página');
 
 
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id`, `email`, `password`, `auth_key`, `nick`, `username`, `register_date`, `confirmed`, `role`, `attempts`, `locked`) VALUES
-(3, 'dj@usal.es', '$2y$13$IXRmKNxfNNMSd7DGQkFo3.aOUovcBEKYby3qojNLF761o4xXfX2.2', 'h5uq58uxdPNhFEmtStMDYoD2a8V60ebT', 'djPiri', 'djPiri', '2025-01-14 12:37:44', 1, '', 0, 0);
 
 --
 -- Índices para tablas comentarios
